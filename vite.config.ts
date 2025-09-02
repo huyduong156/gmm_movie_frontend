30f5951af2454e669345c042c8dfbc418ac1ddc6
huyduong156 <duongquochuy156@gmail.com>
2025-09-02 23:44:29 +0700

    Master | Docker base code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100, // optional: kiểm tra mỗi 100ms
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
